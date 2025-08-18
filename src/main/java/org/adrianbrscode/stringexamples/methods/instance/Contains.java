ff5249963916a0ee91c40013f225a902620788de
AdrianBRScode <penalozaadrian02@gmail.com>
2025-08-18 01:06:20 -0500

    se agrego ejemplo 1 y 2 para explicar como funciona contains() con un CharSequence

--- src/main/java/org/adrianbrscode/stringexamples/methods/instance/Contains.java
+++ src/main/java/org/adrianbrscode/stringexamples/methods/instance/Contains.java
@@ -12,7 +12,41 @@ public class Contains {
         boolean contiene2 = cadena.contains(buscar2);
 
         System.out.printf("la cadena \"%s\" contiene \"%s\": %b\n",cadena,buscar1,contiene1);
-        System.out.printf("la cadena \"%s\" contiene \"%s\": %b",cadena,buscar2,contiene2);
+        System.out.printf("la cadena \"%s\" contiene \"%s\": %b\n",cadena,buscar2,contiene2);
+
+        System.out.println("--------------ejemplo con StringBuilder--------------");
+        StringBuilder sb = new StringBuilder("hola mundo");
+        boolean contiene3 = ejemplo1(cadena,sb);
+        System.out.printf("la cadena \"%s\" contiene \"%s\": %b\n",cadena,sb,contiene3);
+        System.out.println("--------------ejemplo con CharSequence--------------");
+        CharSequence secuenciaChar = "desde java";
+        boolean contiene4 = ejemplo2(cadena,secuenciaChar);
+        System.out.printf("la cadena \"%s\" contiene \"%s\": %b\n",cadena,secuenciaChar,contiene4);
+        //como el metodo es generico podriamos recibir un String, StringBuilder, o cualquier clase
+        //que implemente la interface CharSequence
+
+        //usando StringBuilder
+        boolean contiene5 = ejemplo2(cadena,sb);
+        System.out.printf("la cadena \"%s\" contiene \"%s\": %b\n",cadena,sb,contiene5);
+
+        //usando String
+        boolean contiene6 = ejemplo2(cadena,buscar1);
+        System.out.printf("la cadena \"%s\" contiene \"%s\": %b\n",cadena,buscar1,contiene6);
+
+
     }
+
+    //como recibe un CharSequence puede buscarse otra cosa que no sea un String, como un StringBuilder,
+    //StringBuffer, los cuales son clases que implementan la interface CharSequence
+    public static boolean ejemplo1 (String cadena, StringBuilder sb){
+        return cadena.contains(sb);
+    }
+
+    //se podria recibir tambien un CharSequence y tendriamos un metodo que podria recibir cualquier
+    //objeto de una clase que implemente la interface CharSequence
+    public static boolean ejemplo2 (String cadena, CharSequence cs){
+        return cadena.contains(cs);
+    }
+
 
 }
